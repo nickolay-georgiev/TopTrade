e46881bfabdc61ac1a3451f0b62876c381757a57
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2021-03-29 20:23:55 +0300

    Add trade_history and trade_menu partials and styles for them

---
+++ TopTrade/Web/TopTrade.Web/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopTrade.Web.Areas.User
+{
+    public class HomeController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ TopTrade/Web/TopTrade.Web/Areas/User/Controllers/TradePlatformController.cs
@@ -0,0 +1,19 @@
+﻿namespace TopTrade.Web.Areas.User.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    
+    public class TradePlatformController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
