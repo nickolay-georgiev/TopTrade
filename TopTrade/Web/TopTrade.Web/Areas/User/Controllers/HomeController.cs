32f74ff54540fe45da9276d8aaf6ef4df12b7aba
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2021-04-12 21:14:00 +0300

    Upload userdashboard page done on 80%

--- TopTrade/Web/TopTrade.Web/Areas/User/Controllers/HomeController.cs
+++ TopTrade/Web/TopTrade.Web/Areas/User/Controllers/HomeController.cs
@@ -19,9 +19,9 @@ namespace TopTrade.Web.Areas.User
         [Area("User")]
         public async Task<IActionResult> Index()
         {
-            var getStockByTicker = await this.stockService.GetStockByTicker();
-            var getStockByTicker1 = await this.stockService.GetStockByTicker1();
-            var searchStock = await this.stockService.SearchStock();
+            //var getStockByTicker = await this.stockService.GetStockByTicker();
+            //var getStockByTicker1 = await this.stockService.GetStockByTicker1();
+            //var searchStock = await this.stockService.SearchStock();
 
             return this.View();
         }
